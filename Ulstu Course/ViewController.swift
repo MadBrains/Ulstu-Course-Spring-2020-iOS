e6da130d351f2367819149e9e95c6dc5145d9236
Antol <antol.peshkov@gmail.com>
2020-02-17 18:45:41 +0400

    Initial Commit

---
+++ Ulstu Course/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ulstu Course
+//
+//  Created by Antol Peshkov on 17/02/2020.
+//  Copyright © 2020 Mad Brains. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
