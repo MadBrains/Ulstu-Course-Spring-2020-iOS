dee4184d3b928815fd7ab8809ec7efd8b85d81f7
Antol <antol.peshkov@gmail.com>
2020-02-18 14:46:29 +0400

    1 lecture end

--- Ulstu Course/ViewController.swift
+++ Ulstu Course/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        helloLabel.text = "Hello, World!"
     }
 
-
+    @IBAction func chnageSliderValue(_ sender: UISlider) {
+        print(volumeSlider.value)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("App started!")
+        
+        volumeSlider.value = 0
+    }
 }
 
