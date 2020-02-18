a3b0659d75c14d2dab8a15a636e701b8e50b3973
Antol <antol.peshkov@gmail.com>
2020-02-18 14:49:38 +0400

    Cleanup project

--- Ulstu Course/ViewController.swift
+++ Ulstu Course/ViewController.swift
@@ -9,24 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var helloLabel: UILabel!
-    @IBOutlet weak var volumeSlider: UISlider!
     
-    @IBAction func buttonTap(_ sender: Any) {
-        helloLabel.text = "Hello, World!"
-    }
-
-    @IBAction func chnageSliderValue(_ sender: UISlider) {
-        print(volumeSlider.value)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("App started!")
-        
-        volumeSlider.value = 0
-    }
 }
 
